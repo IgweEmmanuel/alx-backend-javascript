a181561381daef36e67aab7a75ec5b7b5d7c8025
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-07-23 09:23:36 +0100

    add json files and readme

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,14 @@
 // This uses const and let variable declarator instead of var
-
-export function taskFirst () {
+export function taskFirst() {
   const task = 'I prefer const when I can.'
   return task
 }
 
-export function getLast () {
+export function getLast() {
   return ' is okay'
 }
 
-export function taskNext () {
+export function taskNext() {
   let combination = 'But sometimes let'
   combination += getLast()
 
--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,5 +1,4 @@
 // The main is used to run the constants.js
-
 import { taskFirst, taskNext } from './0-constants.js'
 
 console.log(`${taskFirst()} ${taskNext()}`)
