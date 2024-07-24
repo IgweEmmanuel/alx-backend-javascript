fcd7117c5e551e0926fb5407cffff70e1aac8d47
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-07-24 17:25:23 +0000

    promise1

---
+++ 0x00-ES6_basic/0-promise.js
@@ -0,0 +1,12 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      const success = true;
+      if (success) {
+        resolve("Success!");
+      }else {
+        reject("Error!");
+      }
+    }, 1000);
+  });
+}
