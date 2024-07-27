81c55d1ef024ca6d6a15ac238a9e73ffb3ed2b63
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-07-27 04:35:25 +0000

    update classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,10 @@
 import ClassRoom from './0-classroom.js';
 
-export default function initializeRooms(){
-  return [19, 20, 34].map((item) => new ClassRoom(item));
+function initializeRooms() {
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 }
+export default initializeRooms;
