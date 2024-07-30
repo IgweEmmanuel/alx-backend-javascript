5733076fe28de157e13db53c74fc6c284b2324b2
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-07-30 18:23:22 +0000

    update get list student

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,26 +1,25 @@
 /**
  * getlistStudetns - This is the function that prints array of objects
  * Return: It returns array of objects
- */ 
-function getListStudents(){
+ */
+function getListStudents() {
   // returns array objects
-  return([
-    {
-      id: 1,
-      firstName: 'Guillaume',
-      location: 'San Francisco',
-    },
-    {
-      id: 2,
-      firstName: 'James',
-      location: 'Columbia',
-    },
-    {
-      id: 5,
-      firstName: 'Serena',
-      location: 'San Francisco',
-    },
-  ])
+  const one = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+  const two = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+  const three = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+  return [one, two, three];
 }
 
-export default getListStudents
+export default getListStudents;
