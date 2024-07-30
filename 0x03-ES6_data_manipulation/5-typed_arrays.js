b580750fd2e5c67e6b8d58b2aaf6c5b7a2e5d359
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-07-30 21:22:41 +0000

    typed arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,7 +2,7 @@ function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const data = new DataView(buffer);
   if (position < 0 || position > length) {
-    return 'Position outside range';
+    throw new Error('Position outside range');
   }
   data.setInt8(position, value);
   return data;
