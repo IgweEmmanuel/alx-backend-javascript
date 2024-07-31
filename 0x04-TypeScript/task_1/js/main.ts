9c2fa5206d49f020ce4175bfe5e186c9591adad0
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-07-31 19:01:44 +0000

    function interface for Teacher

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -14,3 +14,23 @@ const teacher3: Teacher = {
   location: 'London',
   contract: false,
 };
+
+Interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+Interface printTeacherFunction {
+ (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName:string, lastName:string):string => {
+ return `${firstName.charAt(0)}. ${lastName}`;
+}
